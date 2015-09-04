b8307cee4b1838d153874d157407f173b7ed6f86
Luiz Gustavo <luizgustavo.o@hotmail.com>
2015-09-04 12:56:51 -0300

    Ajuste para seleção de todos os registros de uma grid.

--- backgrid.eachrow.js
+++ backgrid.eachrow.js
@@ -1,14 +1,16 @@
-define(['Backgrid', 'ControlClick'], function (){ // @todo Com a implementação do ControlClick 2.0 esta dependência poderá deixar de existir
+define(['Backgrid', 'ControlClick'], function (){ // @todo Com a implementaÃ§Ã£o do ControlClick 2.0 esta dependÃªncia poderÃ¡ deixar de existir
   eachRow = function(){
 
     var each = Backgrid.Row.extend({
       timeoutDblClick : undefined,
       events: {
-        click: "rowClicked",
-        //dblclick: "alert",
+        "change input[type=checkbox]": "rowClicked",
+        "click" : "rowClicked"
       },
+
       rowClicked: function (e)
       {
+        console.log("ROW CLICKED")
         var self = this;
         var row     = $(e.currentTarget);
         var checked = row.hasClass("selected") ? false : true ;
@@ -22,7 +24,16 @@ define(['Backgrid', 'ControlClick'], function (){ // @todo Com a implementação
         self.model.trigger("backgrid:selected", self.model, checked);
         row.find("input[type=checkbox]").prop('checked',checked);
       },
+
+      /*toggleCheck: function (e)
+      {
+        console.log("TOGGLE CHECKBOX")
+        var row     = $(e.currentTarget);
+        var checkbox = row.find("input[type=checkbox]");
+        var checked = checkbox.is(':checked') ? false : true;
+        checkbox.prop('checked',checked).trigger("change");
+      }*/
     });
     return each;
   }
-});
+})
